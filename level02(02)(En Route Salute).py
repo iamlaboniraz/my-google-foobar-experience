f0ef61c155c0b35b0be80062ec28f9a6cd92e022
Your Name <you@domain.com>
2020-07-12 20:51:22 +0600

    edit

--- level02(02)(En Route Salute).py
+++
@@ -1,9 +0,0 @@
-from time import time
-strat_time = time()
-def answer(s):
-    return sum([s[i:].count("<")for i in range(0,len(s)) if s[i] == ">" ])*2
-print(answer("--->-><-><-->-"))
-end_time = time()
-print(end_time-strat_time)
-
-
